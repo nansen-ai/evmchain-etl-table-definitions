b1f09c2e7cdbef7b3de861483d7c6d3522de3c05
greg <gregoryteo@hotmail.sg>
2022-08-15 13:38:36 +0200

    Add actions/checkout@v2

--- folder_name_test.sh
+++ folder_name_test.sh
@@ -1,12 +1,11 @@
-INVALID_FOLDER_NAMES=$(ls -d ./*/*[[:upper:]]* 2> /dev/null)
+INVALID_FOLDER_NAMES=$(ls -d ./parse/*/*[[:upper:]]* 2> /dev/null)
 if [[ $INVALID_FOLDER_NAMES ]]; then
     for INVALID_FOLDER_NAME in $INVALID_FOLDER_NAMES
     do 
-        echo "::error file=$INVALID_FOLDER_NAME::Folder names must be lowercase"
+        echo "::error ::$INVALID_FOLDER_NAME: Folder names must be lowercase"
     done
     exit 1
 else
     echo "Folder name check success!"
     exit 0
 fi
-
