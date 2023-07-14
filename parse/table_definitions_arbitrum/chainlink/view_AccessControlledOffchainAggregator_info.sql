bcd485b28395a4e84a08ec874f2c7bdd96b7c56c
Eric Ruleman <41025600+ericruleman@users.noreply.github.com>
2023-07-14 04:38:08 -0400

    add chainlink arbitrum feeds (#147)
    Co-authored-by: Eric Ruleman <eric@gauntlet.network>

--- parse/table_definitions_arbitrum/chainlink/view_AccessControlledOffchainAggregator_info.sql
+++ parse/table_definitions_arbitrum/chainlink/view_AccessControlledOffchainAggregator_info.sql
@@ -2,4 +2,13 @@ SELECT * FROM UNNEST([STRUCT('0x46de66f10343b59bacc37df9b3f67cd0ccc121a3' as con
 STRUCT('0xf6328f007a2fdc547875e24a3bc7e0603fd01727' as contract_address, 8 as decimals, '0xdb98056fecfff59d032ab628337a4887110df3db' as proxy_address, 'GMX / USD' as description, 4 as version),
 STRUCT('0x2946220288dbbf77df0030fcecc2a8348cbbe32c' as contract_address, 8 as decimals, '0x50834f3163758fcc1df9973b6e91f0f0f0434ad3' as proxy_address, 'USDC / USD' as description, 4 as version),
 STRUCT('0xb20bd22d3d2e5a628523d37b3ded569598eb649b' as contract_address, 8 as decimals, '0xd0c7101eacbb49f3decccc166d238410d6d46d57' as proxy_address, 'WBTC / USD' as description, 4 as version),
-STRUCT('0x3607e46698d218b3a5cae44bf381475c0a5e2ca7' as contract_address, 8 as decimals, '0x639fe6ab55c921f74e7fac1ee960c0b6293ba612' as proxy_address, 'ETH / USD' as description, 4 as version)])
+STRUCT('0x3607e46698d218b3a5cae44bf381475c0a5e2ca7' as contract_address, 8 as decimals, '0x639fe6ab55c921f74e7fac1ee960c0b6293ba612' as proxy_address, 'ETH / USD' as description, 4 as version),
+STRUCT('0x3c6abda21358c15601a3175d8dd66d0c572cc904' as contract_address, 8 as decimals, '0xad1d5344aade45f43e596773bcc4c423eabdd034' as proxy_address, 'AAVE / USD' as description, 4 as version),
+STRUCT('0x3c6abda21358c15601a3175d8dd66d0c572cc904' as contract_address, 8 as decimals, '0xad1d5344aade45f43e596773bcc4c423eabdd034' as proxy_address, 'AAVE / USD' as description, 4 as version),
+STRUCT('0x9b8ddcf800a7bfcdebad6d65514de59160a2c9cc' as contract_address, 8 as decimals, '0x86e53cf1b870786351da77a57575e79cb55812cb' as proxy_address, 'LINK / USD' as description, 4 as version),
+STRUCT('0x0e9b5c79e005a30bf3fbb4d8cccb6b0082ac5a17' as contract_address, 18 as decimals, '0xb523ae262d20a936bc152e6023996e46fdc2a95d' as proxy_address, 'WSTETH / ETH' as description, 4 as version),
+STRUCT('0x20cd97619a51d1a6f1910ce62d98aceb9a13d5e6' as contract_address, 8 as decimals, '0x0411d28c94d85a36bc72cb0f875dfa8371d8ffff' as proxy_address, 'LUSD / USD' as description, 4 as version),
+STRUCT('0xcb35fe6e53e71b30301ec4a3948da4ad3c65ace4' as contract_address, 8 as decimals, '0x3f3f5df88dc9f13eac63df89ec16ef6e7e25dde7' as proxy_address, 'USDT / USD' as description, 4 as version),
+STRUCT('0xfc06bb03a9e1d8033f87ea6a682cbd65477a43b9' as contract_address, 8 as decimals, '0xc5c8e77b397e531b8ec06bfb0048328b30e9ecfb' as proxy_address, 'DAI / USD' as description, 4 as version),
+STRUCT('0x7aaee6ad40a947a162deab5afd0a1e12be6ff871' as contract_address, 8 as decimals, '0xa14d53bc1f1c0f31b4aa3bd109344e5009051a84' as proxy_address, 'EUR / USD' as description, 4 as version),
+STRUCT('0xc339c4c7c58cb1d964b7b66f846572d5c355441a' as contract_address, 8 as decimals, '0x59644ec622243878d1464a9504f9e9a31294128a' as proxy_address, 'MIMATIC / USD' as description, 4 as version)])
