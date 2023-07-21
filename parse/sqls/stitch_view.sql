62514e1eb7e2f7008fac10c9af3dd639de0f935b
Evgeny Medvedev <medvedev1088@gmail.com>
2023-07-21 18:06:18 +0800

    Remove the sqls folder and ignore it in cloudbuild.yaml (#155)

--- parse/sqls/stitch_view.sql
+++
@@ -1,7 +0,0 @@
-SELECT *
-FROM `{{internal_project_id}}.{{dataset_name}}.{{history_table_name}}`
-WHERE DATE(block_timestamp) <= '{{ds}}'
-UNION ALL
-SELECT *
-FROM `{{internal_project_id}}.{{dataset_name}}.{{table_name}}`
-WHERE DATE(block_timestamp) > '{{ds}}'
