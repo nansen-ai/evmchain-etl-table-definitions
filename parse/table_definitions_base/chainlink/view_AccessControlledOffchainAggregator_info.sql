5e6f9824c81842f616fdc645bff9808c69c22649
Elvis Dai <daishuai323@gmail.com>
2023-08-15 10:54:29 +0800

    Fix. (#178)

--- parse/table_definitions_base/chainlink/view_AccessControlledOffchainAggregator_info.sql
+++ parse/table_definitions_base/chainlink/view_AccessControlledOffchainAggregator_info.sql
@@ -1,6 +1,6 @@
 SELECT * FROM UNNEST([STRUCT('0x08f9654349b33b955133b28e35dbecce9950c219' as contract_address, 18 as decimals, '0x806b4ac04501c29769051e42783cf04dce41440b' as proxy_address, 'CBETH / ETH' as description, 4 as version),
 STRUCT('0x6228a44cd0ec29c3373c9742e4bbaf6f2e536b9a' as contract_address, 8 as decimals, '0x9dda783de64a9d1a60c49ca761ebe528c35ba428' as proxy_address, 'COMP / USD' as description, 4 as version),
-STRUCT('0x21b1e4ea0e9ae2e79932662300eb12a0f90abe59' as contract_address, 8 as decimals, '0x591e79239a7d679378ec8c847e5038150364c78f' as proxy_address, 'DAI / USD' as description, 4 as version)
+STRUCT('0x21b1e4ea0e9ae2e79932662300eb12a0f90abe59' as contract_address, 8 as decimals, '0x591e79239a7d679378ec8c847e5038150364c78f' as proxy_address, 'DAI / USD' as description, 4 as version),
 STRUCT('0x57d2d46fc7ff2a7142d479f2f59e1e3f95447077' as contract_address, 8 as decimals, '0x71041dddad3595f9ced3dccfbe3d1f4b0a16bb70' as proxy_address, 'ETH / USD' as description, 4 as version),
 STRUCT('0x72fc7950a832396720736e7e08d6f74c84c6909a' as contract_address, 8 as decimals, '0x17cab8fe31e32f08326e5e27412894e49b0f9d65' as proxy_address, 'LINK / USD' as description, 4 as version),
 STRUCT('0x79b0e87ff1c40d27a0f941296d70a91cd1553482' as contract_address, 18 as decimals, '0xf586d0728a47229e747d824a939000cf21def5a0' as proxy_address, 'STETH / ETH' as description, 4 as version),
